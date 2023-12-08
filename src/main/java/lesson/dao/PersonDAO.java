5bcaedafc98b289a0eef345504d95eadd97cb77b
Vadik <gloom16@mail.ru>
2023-12-08 16:05:23 +0300

    Сделал CRUD приложение с применением паттерна DAO, реализовал два метода GET для вывода всех people, а так же одного по id. Сделал представления и вывел по запросу пользователя с применением Thymeleaf. В качестве БД использовал ArrayList

---
+++ src/main/java/lesson/dao/PersonDAO.java
@@ -0,0 +1,31 @@
+package lesson.dao;
+
+import lesson.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    // создаем список Person, который будет являться БД
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Ivan"));
+        people.add(new Person(++PEOPLE_COUNT, "Dima"));
+        people.add(new Person(++PEOPLE_COUNT, "Vadim"));
+        people.add(new Person(++PEOPLE_COUNT, "Maxim"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person showById(final int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
