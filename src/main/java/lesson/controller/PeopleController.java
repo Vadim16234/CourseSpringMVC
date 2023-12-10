59809b00426899dd174f232516bb0f536e5dc229
Vadik <gloom16@mail.ru>
2023-12-10 05:48:26 +0300

    Убрал заглушку с методов, чтобы их доработать в dao и проверить работу

--- src/main/java/lesson/controller/PeopleController.java
+++ src/main/java/lesson/controller/PeopleController.java
@@ -55,10 +55,10 @@ public class PeopleController {
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult, @PathVariable("id") int id) {
-//        if (bindingResult.hasErrors()) {
-//            return "people/edit";
-//        }
-//        personDAO.update(id, person);
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+        personDAO.update(id, person);
         return "redirect:/people";
     }
 
